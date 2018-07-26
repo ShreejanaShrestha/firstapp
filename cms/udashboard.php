36851975ba836cfcdc00b636ab2063b7d9dda3ad
shreejanashrestha <sthasrizaana99@hotmail.com>
2018-07-26 12:30:28 +0545

    user dashboard started

--- cms/udashboard.php
+++ cms/udashboard.php
@@ -1,6 +1,5 @@
 <?php 
     include '../inc/usession.php';
-    include '../inc/template_header.php';
     //include '../inc/navigation.php';  
  ?> 
 
@@ -8,11 +7,28 @@
 <html>
 <head>
     <title>user dashboard</title>
+    <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../style.css">
+    <link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
+    <!-- <link href="../assets/css/sb-admin.css" rel="stylesheet"> -->
+
+    <script type="text/javascript" src="../assets/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="../assets/js/jquery.js"></script>
 </head>
 <body>
-    <div class="container">
-    <h2 class="success"><?php echo'Welcome !!! Dear User' ?></h2>
+    <?php include '../inc/uheader.php'; ?>
+    <div style="width:50px;height: 50px;"></div>
+    <div class="col-lg-3">
+        <ul class="navbar navbar-default nav" style="height:650px;">
+            <li><a href="#"><i class="glyphicon glyphicon-dashboard"></i> Dashboard</a></li>
+            <li><a href="#"><i class="glyphicon glyphicon-user"></i> Profile</a></li>
+            <li><a href="#"><i class="glyphicon glyphicon-tasks"></i> Categories</a></li>
+            
+        </ul>    
     </div>
+    <div class="col-lg-8">
+            <h2 class="success"><?php echo'Welcome !!! Dear User' ?></h2>
+        </div>
 </body>
 </html>
 
---
+++ inc/uheader.php
@@ -0,0 +1,10 @@
+<header class="navbar navbar-inverse navbar-fixed-top">
+	<div></div>
+	<div class="container" >
+		<a href="udashboard.php" class="navbar-brand">Dashboard</a>
+		<ul class="nav navbar-nav navbar-right ">
+		<li><a href="udashboard.php">Home</a></li>
+		<li><a href="#">Log Out</a></li>
+		</ul>
+	</div>
+</header>
